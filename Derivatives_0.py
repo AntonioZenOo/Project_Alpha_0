3a20b284a7bedc65a9f340b0bb00420ada83e5a4
AntonioZen <a.gromadskiy2015@gmail.com>
2017-04-16 00:58:20 +0300

    Hello World 2

--- Derivatives_0.py
+++ Derivatives_0.py
@@ -4,22 +4,29 @@ import numpy as np
 import pandas as pd
 from colorama import Fore
 
-data_np =  np.genfromtxt('C:/files/DS_distrib/data_sample.csv',delimiter=',')
-data_df = pd.read_csv('C:/files/DS_distrib/data_sample.csv', sep=',')
-data_np2 = np.recfromcsv('C:/files/DS_distrib/data_sample.csv', delimiter=',', filling_values=np.nan, case_sensitive=True, deletechars='', replace_space=' ')
+data_np =  np.genfromtxt('C:/files/DS_distrib/data_sample.csv',delimiter=',', skip_header = 1)
+#data_df = pd.read_csv('C:/files/DS_distrib/data_sample.csv', sep=',')
+#data_np2 = np.recfromcsv('C:/files/DS_distrib/data_sample.csv', delimiter=',', filling_values=np.nan, case_sensitive=True, deletechars='', replace_space=' ')
 
 #print(Fore.CYAN)
 #print(data_np)
 
-print(Fore.LIGHTYELLOW_EX)
-print(data_df)
+#print(Fore.LIGHTYELLOW_EX)
+#print(data_df)
 
 print(Fore.LIGHTGREEN_EX)
-print(data_np2)
+print(data_np)
 
+#factor_list = ['FACTOR1','FACTOR2','FACTOR3']
+#data_df_3 = data_df[factor_list]
+#print(data_df_3)
 
-data_np2_3 = data_np2[:,[5,6,7]]
-print(data_np2_3)
+#div1 = data_df_3.diff()
+print(type(data_np))
+#exit()
+data_np_3 = data_np[:,4:7]
 print(Fore.CYAN)
-data_df_3 = data_df['FACTOR1','FACTOR2','FACTOR3']
-print(data_df_3)
+print(data_np_3)
+
+#print(Fore.CYAN)
+#print(div1)
