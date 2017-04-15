596150b14d2b655096af6b59501d8a206879e3ce
AntonioZen <a.gromadskiy2015@gmail.com>
2017-04-15 22:51:30 +0300

    Hello World 1

--- Derivatives_0.py
+++ Derivatives_0.py
@@ -4,8 +4,22 @@ import numpy as np
 import pandas as pd
 from colorama import Fore
 
-data_np =  np.genfromtxt('C:/Users/ДиктаторЭ/Desktop/DS_distri/data_sample.csv',delimiter=',')
-data_df = pd.read_csv('C:/Users/ДиктаторЭ/Desktop/DS_distrib/data_sample.csv', sep=',')
+data_np =  np.genfromtxt('C:/files/DS_distrib/data_sample.csv',delimiter=',')
+data_df = pd.read_csv('C:/files/DS_distrib/data_sample.csv', sep=',')
+data_np2 = np.recfromcsv('C:/files/DS_distrib/data_sample.csv', delimiter=',', filling_values=np.nan, case_sensitive=True, deletechars='', replace_space=' ')
 
+#print(Fore.CYAN)
+#print(data_np)
+
+print(Fore.LIGHTYELLOW_EX)
+print(data_df)
+
+print(Fore.LIGHTGREEN_EX)
+print(data_np2)
+
+
+data_np2_3 = data_np2[:,[5,6,7]]
+print(data_np2_3)
 print(Fore.CYAN)
-print(data_np)
+data_df_3 = data_df['FACTOR1','FACTOR2','FACTOR3']
+print(data_df_3)
